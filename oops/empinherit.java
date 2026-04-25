d75f9009a59ad89c336309cce128f41a7a91cd26
192110686 <kanderkrs2003@gmail.com>
2026-04-25 19:53:56 +0530

    25-04-26

--- oops/empinherit.java
+++ oops/empinherit.java
@@ -20,7 +20,21 @@ class Tester extends Employee{
 public class empinherit {
     public static void main(String[] args){
         //create an employee
-Employee e1 = new Employee();
-
+    Employee e1 = new Employee();
+    e1.name="Lokanatham";
+    e1.salary=100000;
+    e1.work();
+    //create a developer
+    Developer d1 = new Developer();
+    d1.name="Vishnu";
+    d1.salary=80000;
+    d1.work();
+    d1.writecode();
+    //create a tester
+    Tester t1 = new Tester();
+    t1.name="Naveen";
+    t1.salary=90000;
+    t1.work();
+    t1.testSoftware();
     }
 }
